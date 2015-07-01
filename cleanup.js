3124a92ad8320b642d7c942eb4b0fedb69b2ad35
Jaken Herman <jch053@shsu.edu>
2015-07-01 16:41:25 -0500

    Update cleanup.js
    replace/split is actually working now.

--- cleanup.js
+++ cleanup.js
@@ -1,12 +1,63 @@
 var a = $(".lwn0").text();
-a = a.split('-')[0];
 
-console.log(a);
+var rep = a.replace(a, a.split(' - ')[0]);
+    $(".lwn0").text(rep);
 
-if (a.substring(0, 3) == "Jul") {
+if (a.substring(0, 3) == "Aug") {
+    var replaced = $(".lwn0").html().replace('Aug', 'August');
+    $(".lwn0").html(replaced);
+}
+
+else if (a.substring(0, 3) == "Sep") {
+    var replaced = $(".lwn0").html().replace('Sep', 'September');
+    $(".lwn0").html(replaced);
+}
+
+else if (a.substring(0, 3) == "Oct") {
+    var replaced = $(".lwn0").html().replace('Oct', 'October');
+    $(".lwn0").html(replaced);
+}
+
+else if (a.substring(0, 3) == "Nov") {
+    var replaced = $(".lwn0").html().replace('Nov', 'November');
+    $(".lwn0").html(replaced);
+}
+
+else if (a.substring(0, 3) == "Dec") {
+    var replaced = $(".lwn0").html().replace('Dec', 'December');
+    $(".lwn0").html(replaced);
+}
+
+else if (a.substring(0, 3) == "Jan") {
+    var replaced = $(".lwn0").html().replace('Jan', 'January');
+    $(".lwn0").html(replaced);
+}
+
+else if (a.substring(0, 3) == "Feb") {
+    var replaced = $(".lwn0").html().replace('Feb', 'February');
+    $(".lwn0").html(replaced);
+}
+
+else if (a.substring(0, 3) == "Mar") {
+    var replaced = $(".lwn0").html().replace('Mar', 'March');
+    $(".lwn0").html(replaced);
+}
+
+else if (a.substring(0, 3) == "Apr") {
+    var replaced = $(".lwn0").html().replace('Apr', 'April');
+    $(".lwn0").html(replaced);
+}
+
+//
+//Dont need to do anything for May. YAY.
+//
+
+else if (a.substring(0, 3) == "Jun") {
+    var replaced = $(".lwn0").html().replace('Jun', 'June');
+    $(".lwn0").html(replaced);
+}
+
+else if (a.substring(0, 3) == "Jul") {
     var replaced = $(".lwn0").html().replace('Jul', 'July');
     $(".lwn0").html(replaced);
-} else if ($("a:contains('Sep')")) {
-    var sepReplace = $(".lwn0").html().replace('Sep', 'September');
-    $(".lwn0").html(sepReplace);
 }
